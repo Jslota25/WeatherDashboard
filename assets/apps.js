89800a4580aa522be8d2416ee75cb022fcb43002
John Slota <jslota17@comcast.net>
2020-03-19 21:37:37 -0500

    Five day forecast

--- assets/apps.js
+++ assets/apps.js
@@ -1,8 +1,31 @@
+//Global variables
+var APIKey = "a63765ff57a3a40184a9d31d6874cc6a";
+var fiveDay = $("#five-day");
+
+//Five-day forecast function
+function getForecast() {
+    var cityName = $("#userInput").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
+    console.log(cityName);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response){
+        console.log(response);
+        var results = response.list;
+
+        for(var i = 0; i < results.length; i++) {
+            
+        }
+        })};
+
+
+
+
+//Search button click event
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
-    var APIKey = "a63765ff57a3a40184a9d31d6874cc6a"
-    var cityName = $("#userInput").val(); 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-
-    console.log(queryURL);
+    getForecast();
+    
 });
