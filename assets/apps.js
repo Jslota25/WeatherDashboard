cacf1d0c797e3fcfe694a80b0aefc5519a948caa
John Slota <jslota17@comcast.net>
2020-03-19 21:54:28 -0500

    UVI Function

--- assets/apps.js
+++ assets/apps.js
@@ -35,10 +35,28 @@ function getWeather() {
     })
 }
 
+//UV index function
+function getUV() {
+    var lat = weatherData.coord.lat;
+    var lon = weatherData.coord.lon;
+    var cityName = $("#userInput").val();
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+    console.log(cityName); 
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response);
+
+    })
+}
+
 
 //Search button click event
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     getForecast();
     getWeather();
+    getUV();
 });
